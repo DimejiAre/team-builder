2957e4c740d60a8bcbc28275e90c7c0230070aff
DimejiAre <olardi2@gmail.com>
2019-09-18 13:10:07 +0100

    add state logic to Form component

--- team-builder/src/components/Form.jsx
+++ team-builder/src/components/Form.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 function Form(props){
 
-    const {teamMembers, setTeamMembers} = props;
+    const {teamFormData, addFormData} = props;
 
     return (
         <form>
             <label htmlFor='nameInput'>Name</label>
-            <input id='nameInput' type='text' />
-            <label htmlFor='emailInput'>Email</label>
-            <input id='emailInput' type='text' />
+            <input value={teamFormData.nameInput} onChange={addFormData} id='nameInput' type='text' />
+            <label htmlFor='email'>Email</label>
+            <input value={teamFormData.emailInput} onChange={addFormData} id='emailInput' type='text' />
             <label htmlFor='roleInput'>Role</label>
-            <input id='roleInput' type='text' />
-            <button>Submit</button>
+            <input value={teamFormData.roleInput} onChange={addFormData} id='roleInput' type='text' />
+            <button onClick={e => e.preventDefault()}>Submit</button>
         </form>
     )
 }
