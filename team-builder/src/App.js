d57312e38bf72ac2623c15af9d625e78af99f391
DimejiAre <olardi2@gmail.com>
2019-09-18 13:33:39 +0100

    refactor team members into components

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,30 +1,10 @@
 import React, { useState } from 'react';
-import './App.css';
 import Form from "./components/Form";
 import uuid from "uuid";
+import {initialTeamMembers, initialFormData} from "./default";
+import TeamMembers from './components/TeamMembers';
 
 function App() {
-
-  const initialFormData = {
-    name: '',
-    email: '',
-    role: ''
-  }
-
-  const initialTeamMembers = [
-    {
-      id: uuid(),
-      name: 'Melqui Pereira',
-      email: 'melquipereira@lambda.com',
-      role: 'frontend engineer'
-    },
-    {
-      id: uuid(),
-      name: 'Austin Walela',
-      email: 'austinwalela@lambda.com',
-      role: 'backend engineer'
-    }
-  ]
 
   const addFormData = e => {
     setTeamFormData({...teamFormData, [e.target.id] : e.target.value});
@@ -45,17 +25,8 @@ function App() {
       teamFormData={teamFormData}
       addFormData={addFormData}
       addTeamMember={addTeamMember}/>
-      <div>
-        <h2>Team Members</h2>
-        {teamMembers.map( member => (
-          <div key={member.id}>
-            <p>Name: {member.name}</p>
-            <p>Email: {member.email}</p>
-            <p>Role: {member.role}</p>
-            <br />
-          </div>
-        ))}
-      </div>
+      <TeamMembers 
+      teamMembers={teamMembers}/>
     </div>
   );
 }
